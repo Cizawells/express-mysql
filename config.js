7eb42498c982c7ce44a67846b48f3d236609d6fd
kwize01 <kwizenterprises@gmail.com>
2022-12-06 20:46:15 +0100

    worked

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "localhost",
+      user: "root",
+      password: "Burundi123..",
+      database: "express_mysql",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
